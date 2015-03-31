bd65b54c3784ac57ecaa131205d3bec7fae6210f
nikitachukov <nikitachukov@me.com>
2015-04-01 02:57:45 +0300

    Удалено излишнее логирование Изменено форматирование ошибок ORA-

--- account/views.py
+++ account/views.py
@@ -18,12 +18,10 @@ def db_auth(username, password):
         try:
 
             dsn_tns = cx_Oracle.makedsn(settings.DATABASES['default']['HOST'], settings.DATABASES['default']['PORT'], settings.DATABASES['default']['NAME'])
-            logger.debug(username)
-            logger.debug(password)
             con = cx_Oracle.connect(username, password[::-1], dsn_tns)
             cursor = con.cursor()
-            cursor.execute('set role lisaro identified by lisaro')
-            cursor.execute('select username,lastname,firstname,middlename,phone,email,job from lisa.usr_django_auth where username=user')
+            cursor.execute("set role lisaro identified by lisaro")
+            cursor.execute("SELECT username,lastname,firstname,middlename,phone,email,job FROM lisa.usr_django_auth WHERE username=user")
             row = cursor.fetchone()
 
             if row:
@@ -35,9 +33,9 @@ def db_auth(username, password):
         except cx_Oracle.DatabaseError as e:
             # logger.error(str(e.args[0]))
             if e.args[0].code == 1017:
-                logger.debug('Please check your credentials %s (ORA-%s)' % (username,str(e.args[0].code)))
+                logger.debug('Please check your credentials %s (ORA-%05d)' % (username, e.args[0].code))
             else:
-                logger.debug('Database connection error: ORA-%s' % (str(e.args[0].code)))
+                logger.debug('Database connection error: ORA-%05d' % e.args[0].code)
 
             return False
     else:
@@ -56,22 +54,22 @@ def login(request):
             logger.debug('Пользователь авторизован в Lisa')
             try:
                 Profile = UserProfile.objects.get(user_id=User.objects.get(username=db_userinfo['USERNAME']).pk)
-                logger.debug('Пользователь %s найден'%Profile.user.username)
-                args['login_error'] = 'Welcome %s %s !!!' % (Profile.user.first_name,Profile.user.last_name)
+                logger.debug('Пользователь %s найден' % Profile.user.username)
+                args['login_error'] = 'Welcome %s %s !!!' % (Profile.user.first_name, Profile.user.last_name)
 
 
             except User.DoesNotExist:
-                Profile=UserProfile.objects.create(user=User.objects.create(username=username,
-                                                                    password=password,
-                                                                    first_name=db_userinfo['FIRSTNAME'],
-                                                                    last_name=db_userinfo['LASTNAME'],
-                                                                    email=db_userinfo['EMAIL']),
-                                           phone=db_userinfo['PHONE'],
-                                           job=db_userinfo['JOB'])
-                args['login_error'] = 'Welcome %s %s !!!' % (Profile.user.first_name,Profile.user.last_name)
-                logger.debug('Пользователь %s создан'%Profile.user.username)
+                Profile = UserProfile.objects.create(user=User.objects.create(username=username,
+                                                                              password=password,
+                                                                              first_name=db_userinfo['FIRSTNAME'],
+                                                                              last_name=db_userinfo['LASTNAME'],
+                                                                              email=db_userinfo['EMAIL']),
+                                                     phone=db_userinfo['PHONE'],
+                                                     job=db_userinfo['JOB'])
+                args['login_error'] = 'Welcome %s %s !!!' % (Profile.user.first_name, Profile.user.last_name)
+                logger.debug('Пользователь %s создан' % Profile.user.username)
         else:
-            logger.debug('Пользователь %s НЕ авторизован в Lisa'%username)
+            logger.debug('Пользователь %s НЕ авторизован в Lisa' % username)
             args['login_error'] = 'Access denied'
 
     return render_to_response("login.html", args)
